5b55a932880ef329507be8445cd1e1210f96bb58
harshavardhan2990 <harshavardhan2990@gmail.com>
2016-05-12 20:59:47 +0530

    'Initial'

---
+++ app/src/main/java/eightloop/com/a101sandwiches/MainActivity.java
@@ -0,0 +1,13 @@
+package eightloop.com.a101sandwiches;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
